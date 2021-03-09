d0478c107a016f65d993cb355572a19fde637a8e
Wild <cadainmiller@gmail.com>
2021-03-08 20:26:40 -0500

    cleaning up code

--- src/App.vue
+++ src/App.vue
@@ -220,31 +220,7 @@
       <div><router-view class="flex-1" /></div>
     </div>
 
-    <!-- <router-view class="flex-1" />
-    <div class="flex-shrink-0 py-12 text-center">
-      <div>
-        <router-link
-          class="font-semibold text-gray-700 hover:underline"
-          to="/dropdown"
-          >Dropdown</router-link
-        >
-        <span class="mx-4 text-gray-500">|</span>
-        <router-link
-          class="font-semibold text-gray-700 hover:underline"
-          to="/modal"
-          >Modal</router-link
-        >
-      </div>
-      <div class="mt-2">
-        <a
-          href="https://github.com/adamwathan/vue-tailwind-examples"
-          class="text-sm font-semibold text-gray-600 hover:underline"
-        >
-          Source on GitHub
-        </a>
-      </div>
-    </div> -->
-    <!-- <portal-target name="modals"></portal-target> -->
+    <portal-target name="modals"></portal-target>
   </div>
 </template>
 
