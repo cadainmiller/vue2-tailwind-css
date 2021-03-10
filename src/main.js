a9a647a996517b91179e9dce432517df54f3d803
Wild <cadainmiller@gmail.com>
2021-03-09 20:49:19 -0500

    Added template

--- src/main.js
+++ src/main.js
@@ -1,13 +1,36 @@
 import Vue from 'vue'
-import PortalVue from 'portal-vue'
 import App from './App.vue'
-import router from './router'
+import Router from 'vue-router'
+
+import Dashboard from '@/components/Dashboard'
+import DashboardHome from '@/pages/Home'
+
+import store from './store'
+
+import '@/assets/css/tailwind.css'
+
 
 Vue.config.productionTip = false
 
-Vue.use(PortalVue)
+Vue.use(Router)
+
+const routes = [
+  { path: '/', redirect: { name: 'DashboardHome' } },
+  { path: '/dashboard', component: Dashboard, children: [
+      { path: '/', redirect: { name: 'DashboardHome' } },
+      { path: 'home', name: 'DashboardHome', component: DashboardHome }
+    ]
+  }
+]
+
+const router = new Router({
+  mode: 'history',
+  routes
+})
 
 new Vue({
+  render: h => h(App),
   router,
-  render: h => h(App)
+  store
 }).$mount('#app')
+
